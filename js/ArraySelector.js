ea37435ffb41ef0b4344f43ccc77107379212805
Aleksey Tsvetkov <antondenikin@gmail.com>
2014-04-11 14:12:43 +0400

    Initial commit

---
+++ js/ArraySelector.js
@@ -0,0 +1,105 @@
+var ArraySelector;
+
+(function() {
+    "use strict";
+
+    if (typeof Array.prototype.filter === "undefined") {
+        Array.prototype.filter = function(array, callback, thisArg) {
+            var result = [];
+            for (var i = 0, max = array.length; i < max; i++) {
+                if (typeof callback === "function" && callback.call(thisArg, array[i])) {
+                    result.push(array[i]);
+                }
+            }
+            return result;
+        };
+        Array.prototype.forEach = function(array, callback, thisArg) {
+            for (var i = 0, max = array.length; i < max; i++) {
+                if (typeof callback === "function") {
+                    callback.call(thisArg, array[i]);
+                }
+            }
+        };
+    }
+
+    Array.prototype.sum = function() {
+        var result = 0;
+        for (var i = 0, max = this.length; i < max; i++) {
+            if (typeof this[i] === "number") {
+                result += this[i];
+            }
+        }
+        return result;
+    };
+
+    ArraySelector = (function() {
+        var calcSum = 10,
+            searchArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            memory = {
+                cache : {},
+                calculate: function(arr, sum) {
+                    var key = arr.join() + sum.toString();
+                    if (typeof this.cache[key] === "undefined") {
+                        this.cache[key] = getVariants();
+                    }
+                    return this.cache[key];
+                }
+            };
+
+        return {
+            getSumCombinations: function(arr, sum) {
+                if ((arr instanceof Array)) {
+                    searchArr = arr;
+                }
+                if (typeof sum === "number" && sum > 0) {
+                    calcSum = sum;
+                }
+                return memory.calculate(searchArr, calcSum);
+            }
+        };
+
+        function getVariants() {
+            var cleanArray = clearArray(searchArr);
+            var totalSum = cleanArray.sum();
+            if (totalSum > calcSum) {
+                return getArrays(cleanArray);
+            } else if (totalSum === calcSum){
+                return [cleanArray];
+            } else {
+                return [];
+            }
+        }
+
+        function clearArray(arr) {
+            return arr.filter(function(item) {
+                return (typeof item === "number" && item > 0);
+            });
+        }
+
+        function getArrays(arr) {
+            var subSets = getSelections(arr);
+            return subSets.filter(function(arrSet){
+                return arrSet.sum() === calcSum;
+            });
+        }
+
+        function getSelections(arr) {
+            var total = arr.length,
+                itemSet = [],
+                power = Math.pow(2, total),
+                result = [];
+
+            for (var i = 0; i < power; i++) {
+                itemSet = [];
+                for (var j = 0; j < total; j++) {
+                    if (i & (1 << j)) {
+                        itemSet.push(arr[j]);
+                    }
+                }
+                result.push(itemSet);
+            }
+            return result;
+        }
+
+    })();
+})();
---
+++ test/ArraySelectorTest.js
@@ -0,0 +1,53 @@
+/*global describe, it, ArraySelector*/
+
+"use strict";
+var fs = require('fs');
+var vm = require('vm');
+var path = './js/ArraySelector.js';
+
+var code = fs.readFileSync(path);
+vm.runInThisContext(code);
+var assert = require("assert");
+
+describe('ArraySelector tests', function(){
+    it('should return empty array', function(){
+        var arr = [1, 2, 3],
+            emptyArr = [],
+            symbolsArr = ["some", "symbols", "and", "words"];
+
+        assert.equal(ArraySelector.getSumCombinations(arr, 10).length, 0);
+        assert.equal(ArraySelector.getSumCombinations(emptyArr, 10).length, 0);
+        assert.equal(ArraySelector.getSumCombinations(symbolsArr, 10).length, 0);
+
+    });
+    it('should return expected count of subsets', function(){
+        var arr = [1, 2, 3],
+            big = [1, 2, 3, 4, 5];
+
+        assert.equal(ArraySelector.getSumCombinations(arr, 10).length, 0);
+        assert.equal(ArraySelector.getSumCombinations(arr, 3).length, 2);
+        assert.equal(ArraySelector.getSumCombinations(big, 5).length, 3);
+    });
+
+    it('should return right arrays', function() {
+        var originalArray = ['1', 1, 2, 3, 4, 5, 6, 7, 8, 0, 12, 9],
+            fiveCombinations = [[5], [2, 3], [1, 4]],
+            sum = 5;
+
+        var fiveResult = ArraySelector.getSumCombinations(originalArray, 5);
+        assert.equal(fiveCombinations.length, fiveResult.length);
+        fiveResult.forEach(function(item){
+            assert.equal(getArraySum(item), sum);
+        });
+
+        function getArraySum(arr) {
+            var result = 0;
+            arr.forEach(function(item){
+                if (typeof item === "number") {
+                    result += item;
+                }
+            });
+            return result;
+        }
+    });
+});
