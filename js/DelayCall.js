cd175784c590d885abd8457ee3bb818ffb0c53f2
Aleksey Tsvetkov <antondenikin@gmail.com>
2014-04-17 13:15:47 +0400

    add timeCall method

--- js/DelayCall.js
+++ js/DelayCall.js
@@ -1,6 +1,12 @@
 (function() {
 
     "use strict";
+
+    /**
+     * Вызов метода не чаще 1 раза в промежуток времени
+     * @param delay
+     * @returns {call}
+     */
     Function.prototype.delayCall = function(delay) {
         delay = clampDelay(delay);
         var func = this;
@@ -11,6 +17,30 @@
             if (time - func.lastRun > delay) {
                 func.lastRun = time;
                 func.apply(func, [].slice.call(arguments));
+            }
+        };
+        return call;
+    };
+
+    /**
+     * Вызов метода count раз за 1 секунду
+     * @param count
+     * @returns {call}
+     */
+    Function.prototype.timeCall = function(count) {
+        var func = this;
+        var compare = new Date().getTime();
+        var total = 0;
+        var call = function() {
+            total++;
+            var time = new Date().getTime();
+            if (time - compare <= 1000) {
+                if (total <= count) {
+                    func.apply(func, [].slice.call(arguments));
+                }
+            } else {
+                compare = time;
+                total = 0;
             }
         };
         return call;
